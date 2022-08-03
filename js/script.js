a772c21b1f6ce2e4617d3babed30e3576d796036
andygardiaz <gardiazandy@gmail.com>
2022-08-04 01:39:00 +0200

    Contenido en homepage y destacados + arreglo menor

---
+++ js/script.js
@@ -0,0 +1,7 @@
+(function () {
+    let whatsappButton = document.querySelector("#whatsapp-button");
+    whatsappButton.addEventListener("click", function(event){
+        event.preventDefault()
+        window.open(`https://wa.me/5211234567890?text=Hola,%20estuve%20viendo%20su%20sitio%20web%20y%20quiero%20entrar%20en%20contacto:%20${window.location}`);
+    })
+})();
