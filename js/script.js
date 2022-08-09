77f09c4e0f2a8d31268012ac2dc9cabcc05c5aa3
andygardiaz <gardiazandy@gmail.com>
2022-08-09 23:30:40 +0200

    Añadiendo número de teléfono de Penayo al contactar WhatsApp

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
     let whatsappButton = document.querySelector("#whatsapp-button");
     whatsappButton.addEventListener("click", function(event){
         event.preventDefault()
-        window.open(`https://wa.me/5211234567890?text=Hola,%20estuve%20viendo%20su%20sitio%20web%20y%20quiero%20entrar%20en%20contacto:%20${window.location}`);
+        window.open(`https://wa.me/5493456434179?text=Hola,%20estuve%20viendo%20su%20sitio%20web%20y%20quiero%20entrar%20en%20contacto:%20${window.location}`);
     })
 })();
 
